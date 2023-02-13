2ac027b0ab573503a0a559677d7adf123511ea10
[Lauret Jason] <[lauret.jason73390@gmail.com]>
2023-02-13 14:43:11 +0100

    fix twitterUserRepo

--- src/main/java/com/hb/guillaume_jason/repository/TwitterUserRepository.java
+++ src/main/java/com/hb/guillaume_jason/repository/TwitterUserRepository.java
@@ -1,6 +1,14 @@
 package com.hb.guillaume_jason.repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hb.guillaume_jason.model.TwitterUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 
